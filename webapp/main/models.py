5f18d78a2467fd41d722ab8838e5e7fefe2b6c77
macSami <sami.ezzerouali@hotmail.com>
2021-03-14 01:54:49 +0000

    fix

--- webapp/main/models.py
+++ webapp/main/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
-from django.contrib.auth.models import User
-from .managers import PersonManager
-
 # Create your models here.
-class Person(User):
-    objects = PersonManager()
-
-    class Meta:
-        proxy = True
-        ordering = ('first_name', )
